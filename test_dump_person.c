c77efd16dccd07da8c623ac700662586c9fad182
FaranAiki <faran.aiki.business@gmail.com>
2026-08-02 13:50:32 +0700

    fix: bazel

--- test_dump_person.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "include/alir/alir.h"
-
-// I can't easily compile this standalone without linking against the whole compiler.
--- test_offset.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stddef.h>
-#include "include/alir/alir.h"
-int main() {
-    printf("offsetof param_count: %zu\n", offsetof(AlirFunction, param_count));
-    printf("offsetof params: %zu\n", offsetof(AlirFunction, params));
-    printf("offsetof blocks: %zu\n", offsetof(AlirFunction, blocks));
-    return 0;
-}
